6e92ffe6a62093619936f9f648c69ba5ffb34cb0
dheatherly <dheatherly@users.noreply.github.com>
2025-07-05 15:19:44 -0400

    Add files via upload

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,41 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
-  };
+    let Total = 0
+    cart.forEach((item) => {
+      const numericCost = parseFloat(item.cost.replace('$', ''))
+      Total += numericCost * (item.quantity || 1)
+    })
+    return Total.toFixed(2)
+  }
 
   const handleContinueShopping = (e) => {
-   
-  };
+    onContinueShopping(e)
+  }
 
-
+  const handleCheckoutShopping = () => {
+    alert('Functionality to be added for future reference')
+  }
 
   const handleIncrement = (item) => {
-  };
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1, }))
+  }
 
   const handleDecrement = (item) => {
-   
-  };
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1, }))
+    }
+  }
 
   const handleRemove = (item) => {
-  };
+    dispatch(removeItem({ name: item.name }))
+  }
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
-  };
+    const numericCost = parseFloat(item.cost.replace('$', ''))
+    return (numericCost * (item.quantity || 1)).toFixed(2)
+  }
 
   return (
     <div className="cart-container">
@@ -64,5 +77,3 @@ const CartItem = ({ onContinueShopping }) => {
 };
 
 export default CartItem;
-
-
